45904392190a32e08f2c358e5279540708ccc982
Jose Theis <main.josesanchez@gmail.com>
2022-02-16 21:35:59 -0400

    init

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { LayoutComponent } from '../layout/layout.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 
 @NgModule({
@@ -14,6 +17,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, LayoutComponent, SidebarComponent, ToolbarComponent]
 })
 export class HomePageModule {}
